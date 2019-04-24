22d7b22bb076ba73dc02b642cb62afd36a684c4a
Franek Zawadzki <franciszekezawadzki@gmail.com>
2019-04-24 10:47:25 +0200

    question

--- question.py
+++ question.py
@@ -1,9 +1,8 @@
 import  csv_data_handler
 
-question_path = '/home/weronika/PycharmProjects/ask-mate-python/sample_data/question.csv'
+question_path = '/home/franek/ask-mate-python/sample_data/question.csv'
 
 
 def get_data():
     return csv_data_handler.read_data(question_path)
 
-#comment
