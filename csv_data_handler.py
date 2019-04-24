2365e64d405a390a73f2a55031b72f9727c725fc
Weronika Walczak <weronikawalczak989@gmail.com>
2019-04-24 10:41:04 +0200

    add question

--- csv_data_handler.py
+++ csv_data_handler.py
@@ -1,6 +1,8 @@
 import csv
 
 answer_path = 'sample_data/answer.csv'
+fieldnames = ['id', 'submisson_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
 
 def read_data(file_path):
     with open(file_path, newline='') as csvfile:
@@ -8,16 +10,11 @@ def read_data(file_path):
         content = []
         for row in reader:
             content.append(row)
-        print(content)
     return content
 
 
 def write_data(file_path, new_row):
     with open(file_path, 'a', newline='') as csvfile:
-        fieldnames = ['submission time', 'view_number', 'vote_number', 'title', 'message', 'image']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
         writer.writerow(new_row)
-
 
