c4b1ff3f426e934046bdb54113cfa51f3560418d
weronikawalczak <weronikawalczak989@gmail.com>
2020-03-24 12:00:00 +0100

    refactor

--- csv_data_handler.py
+++ csv_data_handler.py
@@ -3,7 +3,6 @@ import csv
 answer_path = 'sample_data/answer.csv'
 fieldnames = ['id', 'submisson_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
-
 def read_data(file_path):
     with open(file_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
@@ -12,9 +11,7 @@ def read_data(file_path):
             content.append(row)
     return content
 
-
 def write_data(file_path, data, mode, fieldnames=fieldnames):
     with open(file_path, mode, newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames)
         writer.writerow(data)
-
